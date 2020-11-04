2362c5031ecc3a7c70fd4ddbeec05af60f546842
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2020-11-04 09:31:41 -0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require( 'dotenv' ).config();
 
 module.exports = {
-  PORT: process.env.port,
+  PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   MONGO_DB_NAME: process.env.MONGO_DB_NAME,
   JWT_SECRET: process.env.JWT_SECRET
